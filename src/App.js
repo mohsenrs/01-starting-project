f4a812a1c2b64e42d2bc48c317b7d01f043a5c55
mohsenrs <mohsen8022@gmail.com>
2022-01-24 19:41:43 +0330

    redirect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Products from './pages/Products'
 import Welcome from './pages/Welcome'
-import Home from './pages/Home'
 import Header from './components/Header'
 import ProductDetail from './pages/ProductDetail'
 
@@ -14,7 +13,7 @@ function App() {
       <main>
         <Switch>
           <Route path='/' exact>
-            <Home />
+            <Redirect to='/welcome' />
           </Route>
           <Route path='/welcome'>
             <Welcome />
